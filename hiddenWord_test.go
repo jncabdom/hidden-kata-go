f1afd49ff0311df3ad4d31e0fc3741ccb9b0053a
Nicolás Cabrera <jncabdom@gmail.com>
2021-11-18 16:37:06 +0000

    Refactoring tests

--- hiddenWord_test.go
+++ hiddenWord_test.go
@@ -3,10 +3,28 @@ package main
 import "testing"
 
 func TestHiddenWord(t *testing.T) {
-	const example1 = 147
-	const result1 = "bed"
 
-	if HiddenWord(example1) != result1 {
-		t.Errorf("hiddenWord(%d) => %s", example1, HiddenWord(example1))
+	type args struct {
+		key uint32
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"Key: 147", args{147}, "bed"},
+		{"Key: 637", args{637}, "aid"},
+		{"Key: 7415", args{7415}, "debt"},
+		{"Key: 49632", args{49632}, "email"},
+		{"Key: 942547", args{942547}, "melted"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := HiddenWord(tt.args.key); got != tt.want {
+				t.Errorf("Check() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
